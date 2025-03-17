2d5787b14f516f20316740c724a17e1ef435597b
송상록(Sangrok Song) <157905465+freud-sensei@users.noreply.github.com>
2025-03-17 22:17:58 +0900

    1장 학습내역

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
+// public: 클래스명과 파일명이 같으면, 관례상 class 앞에 public을 붙임
 public class HelloWorld {
+    // public: 누구나 main 메서드에 접근할 수 있음
+    // static: main 메서드는 인스턴스를 생성하지 않아도 실행할 수 있음
+    // void: main 메서드의 리턴 값이 없음
+    // main 메서드는 프로그램을 실행하려면 반드시 필요함
+    // String[]: 문자열을 나타내는 자료형 ([]: 값이 여러 개로 이루어진 배열)
+    // args: String[] 자료형의 변수명
+    public static void main(String[] args) {
+        // 문자열을 화면에 출력하는 내장 메서드
+        System.out.println("Hello World");
+    }
 }
